4b897d8f19374fadcff004f78076726dd76fc84d
Rafael Criado <rafael.marquescriado@gmail.com>
2019-12-02 13:27:31 -0300

    move error 401 to a help error

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -1,4 +1,5 @@
 const MissingParamError = require('./missing-param-error')
+const UnauthorizedError = require('./unauthorized-error')
 
 module.exports =
   class HttpResponse {
@@ -12,6 +13,13 @@ module.exports =
     static serverError () {
       return {
         statusCode: 500
+      }
+    }
+
+    static unauthorizedError () {
+      return {
+        statusCode: 401,
+        body: new UnauthorizedError()
       }
     }
   }
