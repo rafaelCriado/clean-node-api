a34c6b88eb41eb6afffbd368ba589b9db0fd5a58
Rafael Criado <rafael.marquescriado@gmail.com>
2019-12-02 12:14:31 -0300

    move production code to their own files

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,39 +1,5 @@
-class LoginRouter {
-  route (httpRequest) {
-    if (!httpRequest || !httpRequest.body) return HttpResponse.serverError()
-
-    const { email, password } = httpRequest.body
-
-    if (!email) {
-      return HttpResponse.badRequest('email')
-    }
-    if (!password) {
-      return HttpResponse.badRequest('password')
-    }
-  }
-}
-
-class HttpResponse {
-  static badRequest (paramName) {
-    return {
-      statusCode: 400,
-      body: new MissingParamError(paramName)
-    }
-  }
-
-  static serverError () {
-    return {
-      statusCode: 500
-    }
-  }
-}
-
-class MissingParamError extends Error {
-  constructor (paramName) {
-    super(`Missing param: ${paramName}`)
-    this.name = 'MissingParamError'
-  }
-}
+const LoginRouter = require('./login-router')
+const MissingParamError = require('../helpers/missing-param-error')
 
 describe('Login Router', () => {
   test('Should retorn 400 if no email is provided', () => {
@@ -62,7 +28,6 @@ describe('Login Router', () => {
 
   test('Should retorn 500 if no httpRequest is provided', () => {
     const sut = new LoginRouter()
-
     const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
   })
